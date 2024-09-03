fea559c70d2b623d060feb60cbc18eb5a740b570
WesleyRdS <wesleysrw@gmail.com>
2024-09-02 22:05:18 -0300

    Interface com bluetooth

---
+++ build/Desktop_Qt_6_7_2-Debug/ui_mainwindow.h
@@ -0,0 +1,107 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.7.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QListWidget>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QWidget *widget;
+    QHBoxLayout *horizontalLayout;
+    QVBoxLayout *verticalLayout;
+    QPushButton *btn_find;
+    QPushButton *btn_on;
+    QPushButton *btn_off;
+    QListWidget *listWidget;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(749, 450);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        widget = new QWidget(centralwidget);
+        widget->setObjectName("widget");
+        widget->setGeometry(QRect(180, 90, 346, 194));
+        horizontalLayout = new QHBoxLayout(widget);
+        horizontalLayout->setObjectName("horizontalLayout");
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName("verticalLayout");
+        btn_find = new QPushButton(widget);
+        btn_find->setObjectName("btn_find");
+
+        verticalLayout->addWidget(btn_find);
+
+        btn_on = new QPushButton(widget);
+        btn_on->setObjectName("btn_on");
+
+        verticalLayout->addWidget(btn_on);
+
+        btn_off = new QPushButton(widget);
+        btn_off->setObjectName("btn_off");
+
+        verticalLayout->addWidget(btn_off);
+
+
+        horizontalLayout->addLayout(verticalLayout);
+
+        listWidget = new QListWidget(widget);
+        listWidget->setObjectName("listWidget");
+
+        horizontalLayout->addWidget(listWidget);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 749, 20));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        btn_find->setText(QCoreApplication::translate("MainWindow", "Find", nullptr));
+        btn_on->setText(QCoreApplication::translate("MainWindow", "On", nullptr));
+        btn_off->setText(QCoreApplication::translate("MainWindow", "Off", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
